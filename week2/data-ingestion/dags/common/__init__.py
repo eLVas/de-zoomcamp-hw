f5d109d405338c996c6349c5009060333384b96d
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2022-02-03 17:47:45 +0100

    Add zones dag

--- week2/data-ingestion/dags/common/__init__.py
+++ week2/data-ingestion/dags/common/__init__.py
@@ -9,18 +9,25 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 start_date = datetime.fromisoformat("2019-01-01")
 
-dataset_base_url = "https://s3.amazonaws.com/nyc-tlc/trip+data/"
+dataset_base_url = "https://s3.amazonaws.com/nyc-tlc/"
+dataset_base_url_trip_data = dataset_base_url + "trip+data/"
+dataset_base_url_misc = dataset_base_url + "misc/"
+
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 date_string_month_template = "{{ dag_run.logical_date.strftime('%Y-%m') }}"
 
-default_args_rides_ingestion = {
+default_args = {
     "owner": USER,
-    "start_date": start_date,
-    "schedule_interval": "@monthly",
-    "catchup": True,
     "depends_on_past": False,
     "max_active_runs": 1,
     "retries": 1,
     "tags": ['dtc-de'],
+}
+
+default_args_rides_ingestion = {
+    **default_args,
+    "start_date": start_date,
+    "schedule_interval": "@monthly",
+    "catchup": True,
 }
--- week2/data-ingestion/dags/zones_data_ingestion_dag.py
+++ week2/data-ingestion/dags/zones_data_ingestion_dag.py
@@ -1,29 +1,31 @@
+
+# This is an airflow DAG
+
 from datetime import datetime
 
 from airflow import DAG
+from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+import common
 from common.tasks import convert_to_parquet, upload_to_gcs
-from common import *
 
+dataset_name = "zones"
+base_file_name = "taxi+_zone_lookup"
 
-def define_ny_taxi_data_ingestion_dag(dag_id, dataset_name, dataset_base_file_name, default_args):
-
-    dataset_base_file_url = dataset_base_url_trip_data + dataset_base_file_name
-
-    dag = DAG(
-        dag_id=dag_id,
-        default_args=default_args,
-    )
-
-    # NOTE: DAG declaration - using a Context Manager (an implicit way)
-    with dag:
-        local_file_path = path_to_local_home + '/' + dataset_base_file_name + date_string_month_template
+with DAG(
+        dag_id="zones_data_ingestion",
+        default_args=common.default_args,
+        schedule_interval=None,
+        start_date=days_ago(0),
+        catchup=False,
+    ) as dag:
+        local_file_path = common.path_to_local_home + '/' + base_file_name
         local_file_path_csv = local_file_path + ".csv"
         local_file_path_parquet = local_file_path + ".parquet"
 
-        url = dataset_base_file_url + date_string_month_template + ".csv"
+        url = common.dataset_base_url_misc + base_file_name + ".csv"
         download_dataset_task = BashOperator(
             task_id="download_dataset_task",
             bash_command=f"curl -sSLf {url} > {local_file_path_csv}"
@@ -37,14 +39,13 @@ def define_ny_taxi_data_ingestion_dag(dag_id, dataset_name, dataset_base_file_na
             },
         )
 
-        gcs_file_path = "raw/" + dataset_name + \
-                        "/{{ dag_run.logical_date.year }}/{{ dag_run.logical_date.strftime('%m') }}.parquet"
+        gcs_file_path = "raw/" + dataset_name + "/data.parquet"
 
         local_to_gcs_task = PythonOperator(
              task_id="local_to_gcs_task",
              python_callable=upload_to_gcs,
              op_kwargs={
-                 "bucket": BUCKET,
+                 "bucket": common.BUCKET,
                  "object_name": gcs_file_path,
                  "local_file": local_file_path_parquet,
              },
@@ -56,5 +57,3 @@ def define_ny_taxi_data_ingestion_dag(dag_id, dataset_name, dataset_base_file_na
         )
 
         download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> cleanup
-
-        return dag
